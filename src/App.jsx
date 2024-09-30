c1d9409396659cb0efb789f0300ef7998e451af9
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2024-09-30 12:15:27 -0300

    idem

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element = {<Home/>} />
+                <Route path="/foster-amazing-3d-portfolio/home" element = {<Home/>} />
                 <Route path="/about" element = {<About/>} />
                 <Route path="/projects" element = {<Projects/>} />
                 <Route path="/contact" element = {<Contact/>} />
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base:'/'
+  base:'/foster-amazing-3d-portfolio/home'
 })
