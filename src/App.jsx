b078806532e004e9a330ea926f84c6e124cf7a6d
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2024-09-30 12:12:46 -0300

    fix(): idem

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/home" element = {<Home/>} />
+                <Route path="/" element = {<Home/>} />
                 <Route path="/about" element = {<About/>} />
                 <Route path="/projects" element = {<Projects/>} />
                 <Route path="/contact" element = {<Contact/>} />
